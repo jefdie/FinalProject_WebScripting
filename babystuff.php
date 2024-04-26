147e8b5477899feb6266c80facafc7087aeeb15f
Pittomic <157560832+Pittomic@users.noreply.github.com>
2024-04-25 20:12:02 -0400

    1st Main Commit

---
+++ babystuff.php
@@ -0,0 +1,32 @@
+<?php
+
+$host = 'localhost';    // Change as necessary
+  $data = 'grocery'; // Change as necessary
+  $user = 'admin';         // Change as necessary
+  $pass = 'your_password';        // Change as necessary
+  $chrs = 'utf8mb4';
+  $attr = "mysql:host=$host;dbname=$data";"charset=$chrs";
+  $opts =
+  [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+?>
+
+<?php
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=grocery", $user, $pass);
+  
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+?>
