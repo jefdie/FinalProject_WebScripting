ef4ee31ba5d1c464018d46186ab4e8c4f562e36b
Pittomic <157560832+Pittomic@users.noreply.github.com>
2024-04-27 16:06:54 -0400

    1st Index change

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>World of Groceries</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles.css">
+  </head>
+  <body>
+    <header>
+        <a href="index.php" class="logo"><img src="assets/CIT336 Logo.png"></a>
+        <a href="login.php" class="account"><img src="assets/AccountIcon.png">Log In</a>
+        <br>
+        <h5><a href="babystuff.php">Babies</a></h5>
+        <h5><a href="Beverage.php">Beverage</a></h5>
+        <h5><a href="Meat.php">Meat</a></h5>
+        <h5><a href="Pets.php">Pets</a></h5>
+    </header>
+    <main>
+        <h1>Welcome to the World of Groceries</h1><br><br>
+        <h5>Check out all sorts of products from different stores to find the best buying option for you</h5>  
+    </main>
+  </body>
+</html>
--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT babyfood, diapers,wipes FROM BabyStuff";
+$sql = "SELECT babyfood, diapers, wipes FROM BabyStuff";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
