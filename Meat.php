6250a959a87860a082f0f706f51f91f36829164b
Pittomic <157560832+Pittomic@users.noreply.github.com>
2024-05-01 22:03:01 -0400

    Header adjustments

--- Meat.php
+++ Meat.php
@@ -1,2 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>World of Groceries</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles.css" />
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        <a href="index.php"
+          ><img src="assets/CIT336 Logo.png" width="154" height="80.7"
+        /></a>
+      </div>
+      <input class="search" type="text" placeholder="Search..." />
+      <div class="loginbutton">
+        <a href="login.php" class="account"
+          ><img src="assets/AccountIcon.png" width="80.7" height="80.7" />Log
+          In</a
+        >
+      </div>
+      <br />
+      <h5>
+        <ul>
+          <li><a href="babystuff.php">Babies</a></li>
+          <li><a href="Beverage.php">Beverage</a></li>
+          <li><a href="Meat.php">Meat</a></li>
+          <li><a href="Pets.php">Pets</a></li>
+        </ul>
+      </h5>
+    </header>
+    <main>
 
-<a href="index.php"><button>Go to Main Page</button></a>
+    </main>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>World of Groceries</title>
-    <link rel="stylesheet" type="text/css" href="assets/styles.css">
+    <link rel="stylesheet" type="text/css" href="assets/styles.css" />
   </head>
   <body>
     <header>
-        <a href="index.php" class="logo"><img src="assets/CIT336 Logo.png"></a>
-        <a href="login.php" class="account"><img src="assets/AccountIcon.png">Log In</a>
-        <br>
-        <h5><a href="babystuff.php">Babies</a></h5>
-        <h5><a href="Beverage.php">Beverage</a></h5>
-        <h5><a href="Meat.php">Meat</a></h5>
-        <h5><a href="Pets.php">Pets</a></h5>
+      <div class="logo">
+        <a href="index.php"
+          ><img src="assets/CIT336 Logo.png" width="154" height="80.7"
+        /></a>
+      </div>
+      <input class="search" type="text" placeholder="Search..." />
+      <div class="loginbutton">
+        <a href="login.php" class="account"
+          ><img src="assets/AccountIcon.png" width="80.7" height="80.7" />Log
+          In</a
+        >
+      </div>
+      <br />
+      <h5>
+        <ul>
+          <li><a href="babystuff.php">Babies</a></li>
+          <li><a href="Beverage.php">Beverage</a></li>
+          <li><a href="Meat.php">Meat</a></li>
+          <li><a href="Pets.php">Pets</a></li>
+        </ul>
+      </h5>
     </header>
     <main>
-        <h1>Welcome to the World of Groceries</h1><br><br>
-        <h5>Check out all sorts of products from different stores to find the best buying option for you</h5>  
+      <h1>Welcome to the World of Groceries!</h1>
+      <br /><br />
+      <h5>
+        Check out all sorts of products from different stores to find the best
+        buying option for you!
+      </h5>
     </main>
   </body>
 </html>
