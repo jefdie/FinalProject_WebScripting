5babf27d9e990d69222c2a160e7edd07b026a173
Pittomic <157560832+Pittomic@users.noreply.github.com>
2024-05-10 21:04:21 -0400

    Deleted 2nd html in index

--- index.php
+++ index.php
@@ -45,8 +45,6 @@
         buying option for you!
       </h5>
     </main>
-  </body>
-</html>
 
 <?php
 session_start();
@@ -58,14 +56,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-</head>
-<body>
     <h1>Welcome, <?php echo $_SESSION["username"]; ?>!</h1>
     <a href="logout.php">Logout</a>
 </body>
